b855e1830bb62319abad6a3370dd8f774f68bde2
Matheus Sales <matheus.carvalho.sales@gmail.com>
2023-09-29 09:05:56 -0300

    chore: add lib date-fns

---
+++ src/utils/dateUtils.ts
@@ -0,0 +1,46 @@
+import {parseISO, differenceInSeconds, format} from 'date-fns';
+
+function formatRelative(dateIso: string): string {
+  const date = parseISO(dateIso);
+  const now = new Date();
+
+  const diffInSeconds = differenceInSeconds(now, date);
+
+  if (diffInSeconds < 60) {
+    return `${diffInSeconds} s`;
+  }
+
+  const diffInMinutes = Math.round(diffInSeconds / 60);
+
+  if (diffInMinutes < 60) {
+    return `${diffInMinutes} m`;
+  }
+
+  const diffInHours = Math.round(diffInMinutes / 60);
+
+  if (diffInHours < 24) {
+    return `${diffInHours} h`;
+  }
+
+  const diffInDays = Math.round(diffInHours / 24);
+
+  if (diffInDays < 30) {
+    return `${diffInDays} d`;
+  }
+
+  const diffInWeeks = Math.round(diffInDays / 7);
+
+  if (diffInWeeks < 4) {
+    return `${diffInWeeks} w`;
+  }
+
+  const diffInMonths = Math.round(diffInDays / 30);
+
+  if (diffInMonths < 12) {
+    return `${diffInMonths} mo`;
+  }
+
+  return format(date, 'dd/MM/yyyy');
+}
+
+export const dateUtils = {formatRelative};
